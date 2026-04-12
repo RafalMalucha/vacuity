82b068b47d14ffb4046c09d7fc3215293198de8d
Rafal <rafal.z.malucha@gmail.com>
2026-04-13 00:58:01 +0200

    merge

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <stdio.h>
 
+#include "core/entity.h"
+
 int main()
 {
+  Entity entity1;
+
   printf("hello, vacuity\n");
+
+  std::cout << entity1.id << std::endl;
 
   std::cin.get();
 
