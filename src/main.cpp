07b160770aac44d0834a9c232f4800e733118156
Rafal <rafal.z.malucha@gmail.com>
2026-04-13 01:17:55 +0200

    entity test

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
+#include <vector>
 #include <stdio.h>
 
 #include "core/entity.h"
 
 int main()
 {
+  Entity entity0;
   Entity entity1;
+  Entity entity2;
+  Entity entity3;
+
+  std::vector<Entity> entities = {entity0, entity1, entity2, entity3};
 
   printf("hello, vacuity\n");
 
-  std::cout << entity1.id << std::endl;
+  for(Entity entity : entities)
+  {
+    std::cout << entity.id << std::endl;
+  }
 
   std::cin.get();
 
